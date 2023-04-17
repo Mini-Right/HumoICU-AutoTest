4b20d9ad6fb242c36b3e8bc4e024670a028832c4
Mini-Right <www@anyu.wang>
2023-04-18 00:44:54 +0800

    init

---
+++ backend/main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time        : 2023/4/18 00:44
+# @Author      : Mini-Right
+# @Email       : www@anyu.wang
+# @File        : main.py
+# @Software    : PyCharm
+# @Description :
