b7cd48515d36782b89c0d5a16d62238b9d26113c
Mini-Right <www@anyu.wang>
2023-04-26 02:46:38 +0800

    feat: 密码生成

---
+++ backend/humo/plugins/crypt/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time        : 2023/4/26 00:15
+# @Author      : Mini-Right
+# @Email       : www@anyu.wang
+# @File        : __init__.py.py
+# @Software    : PyCharm
+# @Description :
+from humo.plugins.crypt.password import Password
+
+__all__ = ['Password']
---
+++ backend/humo/plugins/crypt/password.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time        : 2023/4/26 00:15
+# @Author      : Mini-Right
+# @Email       : www@anyu.wang
+# @File        : password.py
+# @Software    : PyCharm
+# @Description :
+from passlib.context import CryptContext
+
+
+class Password(object):
+    def __init__(self):
+        self.pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+    def get_password_hash(self, password: str):
+        return self.pwd_context.hash(password)
+
+    def verify_password(self, plain_password: str, hashed_password: str):
+        return self.pwd_context.verify(plain_password, hashed_password)
